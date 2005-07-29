aa83cf3714fedd4d7835aa880eb650efd70e990e
locatech <>
2005-07-29 20:19:53 +0000

    - beautified price upload page

--- www/price-lists/upload-prices.tcl
+++ www/price-lists/upload-prices.tcl
@@ -19,7 +19,7 @@ set page_title "[_ intranet-trans-invoices.lt_Upload_Client_Prices_]"
 
 set context_bar [im_context_bar [list "/intranet/companies/" "[_ intranet-trans-invoices.Clients]"] "[_ intranet-trans-invoices.Upload_CSV]"]
 
-set page_content "
+set page_body "
 <form enctype=multipart/form-data method=POST action=upload-prices-2.tcl>
 [export_form_vars company_id return_url]
                     <table border=0>
